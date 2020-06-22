f5ebd3a1f6dd23996106fe7ef44e2c5caf49768c
saifete11ruet <saifete11ruet@gmail.com>
2020-06-22 21:48:50 +0600

    Optimization

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,8 +1,25 @@
 <template>
   <client-only>
-    <v-layout column justify-center align-center>
-      <v-flex xs12 sm8 md6>
-        List of all available Rents List
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm12 md12>
+        <v-card>
+          <v-card-title primary-title class="text-center">
+            List of all available Rents List
+          </v-card-title>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-data-table
+                  :headers="rentHeaders"
+                  :items="rents"
+                  hide-default-footer
+                  class="elevation-0"
+                >
+                </v-data-table>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </client-only>
@@ -13,7 +30,69 @@ export default {
   name: 'MainPage',
   data() {
     return {
-      loginDialog: false,
+      rents: [],
+      rentHeaders: [
+        {
+          text: 'SL',
+          value: 'SL',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Owner',
+          value: 'OwnerName',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'House Name',
+          value: 'HouseName',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Total Floors',
+          value: 'total_floor_no',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Rented Floor',
+          value: 'rented_floor_no',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Rented Flat',
+          value: 'rented_flat_no',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Address',
+          value: 'address',
+          align: 'center',
+          sortable: false,
+        },
+        // {
+        //   text: 'Gas',
+        //   value: 'gas_available',
+        //   align: 'center',
+        //   sortable: false,
+        // },
+        // {
+        //   text: 'Electricity',
+        //   value: 'electricity_available',
+        //   align: 'center',
+        //   sortable: false,
+        // },
+        // {
+        //   text: 'Water',
+        //   value: 'water_available',
+        //   align: 'center',
+        //   sortable: false,
+        // },
+      ],
     }
   },
 }
